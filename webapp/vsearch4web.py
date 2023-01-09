2c9f45286ed157521223327dc0d40bd60a02da52
mprzybysz <mprzybysz.dev@gmail.com>
2023-01-09 21:45:23 +0100

    end of chapter seven

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, escape
 from searchmodule import search_for_letters
-import datetime;
+import datetime
 
 app = Flask(__name__)
 
@@ -12,9 +12,15 @@ def entry_page() -> 'html':
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(datetime.datetime.now(), req.form, req.remote_addr, req.user_agent, req.user_agent, res, file=log, sep='|')
-
+    dbconfig = {'host': '127.0.0.1', 'port': '3307', 'user': 'vsearch', 'password': 'password123', 'database': 'vsearchlogDB', }
+    import mysql.connector
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """INSERT INTO log (phrase, letters, ip, browser_string, results) VALUES (%s, %s, %s, %s, %s)"""
+    cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, str(req.user_agent.browser), res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
