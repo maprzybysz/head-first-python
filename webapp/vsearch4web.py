300bd47f315866665f731f2342e08e677442ebb6
mprzybysz <mprzybysz.dev@gmail.com>
2023-01-16 18:21:19 +0100

    fix

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -10,11 +10,6 @@ app.config['dbconfig'] = {'host': '127.0.0.1', 'port': '3307', 'user': 'vsearch'
 app.secret_key = 'NigdyNieZgadnieszMojegoTajnegoKlucza'
 
 
-@app.route('/')
-@app.route('/entry')
-def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to the search4letters website!')
-
 @app.route('/login')
 def do_login() -> str:
     session['logged_in'] = True
@@ -27,32 +22,51 @@ def do_logout() -> str:
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-   
     with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """INSERT INTO log (phrase, letters, ip, browser_string, results) VALUES (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, str(req.user_agent), res, ))
+        _SQL = """insert into log
+        (phrase, letters, ip, browser_string, results)
+        values
+        (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form['phrase'],
+        req.form['letters'],
+        req.remote_addr,
+        req.user_agent.browser,
+        res, ))
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    title = 'Here are your results'
+    title = 'Oto Twoje wyniki:'
     results = str(search_for_letters(phrase, letters))
     log_request(request, results)
-    return render_template('results.html', the_phrase = phrase, the_letters=letters, the_title=title, the_results=results,)
+    return render_template('results.html',
+    the_title=title,
+    the_phrase=phrase,
+    the_letters=letters,
+    the_results=results,)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+    the_title='Witamy na stronie internetowej search4letters!')
 
 
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
-
     with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """SELECT phrase, letters, ip, browser_string, results FROM log"""
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
         cursor.execute(_SQL)
         contents = cursor.fetchall()
-        titles = ('Fraza', 'Litery', 'Adres klienta', 'Agent użytkownika', 'Wyniki')
-
-    return render_template('viewlog.html', the_title='Widok logu', the_row_titles=titles, the_data=contents,)
+    titles = ('Fraza' , 'Litery' , 'Adres klienta', 'Agent użytkownika', 'Wyniki')
+    return render_template('viewlog.html',
+    the_title='View Log',
+    the_row_titles=titles,
+    the_data=contents,)
 
 if __name__ == '__main__':
     app.run(debug=True)
